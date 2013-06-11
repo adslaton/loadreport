7e073df65ec8c092da457ff9d6f01ca0742cc24a
A.D. Slaton <ad.slaton@turner.com>
2013-06-11 13:10:02 -0400

    Formating changes to speedreport.js

--- speedreport.js
+++ speedreport.js
@@ -1,42 +1,55 @@
-var fs = require('fs');
-var page = require('webpage').create(),
-    t, address=phantom.args[0]||encodeURI(address)+'.txt', output=phantom.args[1]||encodeURI(address)+'.jpg',requests={}, responses={}, pageInfo={url:address, assets:[]};
+var fs = require('fs'),
+    page = require('webpage').create(),
+    address = phantom.args[0] || encodeURI(address) + '.txt',
+    output = phantom.args[1] || encodeURI(address)+'.jpg',
+    requests = {},
+    responses = {},
+    pageInfo = {url: address, assets: []},
+    t;
 
 if (phantom.args.length === 0) {
     //console.log('Usage: loadspeed.js <some URL>');
     phantom.exit();
 }
+
 page.onResourceRequested = function (r) {
     //console.log('Request ' + JSON.stringify(r, undefined, 4));
-    if(r)requests[r.id]=r;
+    if (r) {
+        requests[r.id] = r;
+    }
 };
+
 page.onResourceReceived = function (r) {
+    var i;
+
     //console.log('Receive ' + JSON.stringify(r, undefined, 4));
     //phantom.exit();
-    if(r && !(r.id in responses)){
-        responses[r.id]=r;
+    if (r && !(r.id in responses)) {
+        responses[r.id] = r;
         //console.log('/* '+r.stage+' */');
     } else {
         //r.bodySize=responses[r.id].bodySize;
-        for(var i in responses[r.id]){
-            if(responses[r.id].hasOwnProperty(i) && !(i in r)){
-                r[i]=responses[r.id][i];
+        for (i in responses[r.id]){
+            if (responses[r.id].hasOwnProperty(i) && !(i in r)) {
+                r[i] = responses[r.id][i];
             }
         }
-        r.received=responses[r.id].time;
+        r.received = responses[r.id].time;
         pageInfo.assets.push({
-            request:requests[r.id],
-            response:r
+            request: requests[r.id],
+            response: r
         });
     }
     //var start=Date.parse(requests[r.id].time),end=Date.parse(r.time)
     //console.log((end-start)+'ms',r.status, r.url.substring(0,50));
 };
+
 t = Date.now();
 address = phantom.args[0];
+
 page.open(address, function (status) {
-    pageInfo.requestTime=t;
-    pageInfo.responseTime=Date.now();
+    pageInfo.requestTime = t;
+    pageInfo.responseTime = Date.now();
     if (status !== 'success') {
         console.log('/* FAIL to load the address */');
     } else {
@@ -56,22 +69,20 @@ page.open(address, function (status) {
 });
 
 function printToFile(data) {
-    var f, g,html, myfile, fileid, myjson, jspath,
+    var f, g, html, myfile, fileid, myjson, jspath,
         keys = [], values = [], extension = 'html';
 
-    if(!phantom.args[1]){
+    if (!phantom.args[1]) {
         fileid = phantom.args[0].replace('http://','').replace('https://','').replace(/\//g,'');
         fileid = fileid.split('?')[0];
-    }else{
+    } else {
         fileid = phantom.args[1];
     }
 
     myfile = 'speedreports/' + fileid + '.' + extension;
     myjson = fileid;
 
-
-
-    if(fs.exists(myfile)){
+    if (fs.exists(myfile)) {
         fs.remove(myfile);
     }
     //write the headers and first line
@@ -80,19 +91,19 @@ function printToFile(data) {
         g = fs.open('speedreports/' + myjson  + '.js', "w");
         g.writeLine('var reportdata = ' + data + ';');
         g.close();
-        g = g = fs.open('speedreports/' + myjson  + '.json', "w");
+        g = fs.open('speedreports/' + myjson  + '.json', "w");
         g.writeLine(data);
         g.close();
 
-        if(!fs.exists('speedreport.html')){
+        if (!fs.exists('speedreport.html')) {
             html = fs.read('loadreport/speedreport.html');
-        }else{
+        } else {
             html = fs.read('speedreport.html');
         }
         f.writeLine(html);
-        if(phantom.args[1]){
+        if (phantom.args[1]) {
             f.writeLine('<script src=\"\/rest\/performance\/js\?uuid\=' + myjson + '\"></script>');
-        }else{
+        } else {
             f.writeLine('<script src=\"' + myjson + '\.js"></script>');
         }
 
@@ -109,6 +120,5 @@ function printToFile(data) {
     } catch (e) {
         console.log("problem writing to file",e);
     }
-
 }
 
